96ba4bb6bf67e9247d756154d9cf914241c458a6
ratioratio <83388579+ratioratio@users.noreply.github.com>
2022-01-05 16:09:57 -0700

    Add files via upload

---
+++ niggersareblack.py
@@ -0,0 +1,231 @@
+import random, string
+from os import system
+import time
+from tqdm import tqdm
+import ctypes
+import socket
+from time import sleep
+import replit
+import random
+import sys
+import pyfiglet
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+ctypes.windll.kernel32.SetConsoleTitleW("ddoxer")
+print(f"{Fore.BLUE}")
+
+
+nigger = pyfiglet.figlet_format("Ddoxing tool")
+print(nigger)
+time.sleep(2)
+print("")
+time.sleep(0.5)
+print("Follow instructions to commence ddox \n")
+time.sleep(0.2)
+print(f"{Fore.RED}")
+
+input('Name of person to ddox: ')
+
+def choose_word():
+    list_of_words = []
+    with open('codes.txt') as f:
+        line = f.readline().strip()
+        while line:
+            list_of_words.append(line)
+            line=f.readline().strip()
+    chosen_word = random.choice(list_of_words)
+    return chosen_word
+
+def main():
+    print(choose_word())
+
+main()
+
+print("finding persons age:")
+time.sleep(2)
+print(random.randint(10, 20))
+
+print("checking databases breaches:")
+time.sleep(0.3)
+print("found leak")
+time.sleep(0.3)
+print("unhashing")
+
+import time
+for i1 in tqdm(range(1)):
+    for i2 in tqdm(range(300), leave=False):
+        # do something, e.g. sleep
+        time.sleep(0.01)
+
+print("passwords found")
+print("sending passwords from server in 5 seconds")
+
+
+def choose_word():
+    list_of_words = []
+    with open('nigga.txt') as f:
+        line = f.readline().strip()
+        while line:
+            list_of_words.append(line)
+            line=f.readline().strip()
+    chosen_word = random.choice(list_of_words)
+    return chosen_word
+
+def main():
+    print(choose_word())
+
+main()
+
+   
+
+print("Finding Ip")
+
+import time
+for i1 in tqdm(range(1)):
+    for i2 in tqdm(range(100), leave=False):
+        # do something, e.g. sleep
+        time.sleep(0.01)
+
+ip = ".".join(map(str, (random.randint(0, 255) 
+                        for _ in range(4))))
+
+print(ip)
+time.sleep(1)
+
+print("finding relatives names")
+
+
+def choose_word():
+    list_of_words = []
+    with open('niggas.txt') as f:
+        line = f.readline().strip()
+        while line:
+            list_of_words.append(line)
+            line=f.readline().strip()
+    chosen_word = random.choice(list_of_words)
+    return chosen_word
+
+def main():
+    print(choose_word())
+
+main()
+
+print("found name")
+print("finding other names")
+def choose_word():
+    list_of_words = []
+    with open('niggas.txt') as f:
+        line = f.readline().strip()
+        while line:
+            list_of_words.append(line)
+            line=f.readline().strip()
+    chosen_word = random.choice(list_of_words)
+    return chosen_word
+
+def main():
+    print(choose_word())
+
+main()
+print("found name")
+print("finding other names")
+def choose_word():
+    list_of_words = []
+    with open('niggas.txt') as f:
+        line = f.readline().strip()
+        while line:
+            list_of_words.append(line)
+            line=f.readline().strip()
+    chosen_word = random.choice(list_of_words)
+    return chosen_word
+
+def main():
+    print(choose_word())
+
+main()
+
+time.sleep(5)
+
+replit.clear()
+num=input("amount of swat teams to send: ")
+
+print("swat teams being sent")
+
+
+
+
+
+
+replit.clear()
+
+input("How many pizzas to send: ")
+print("pizzas sent")
+
+replit.clear()
+
+print("""
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/////
+,                                                                          .,,,,
+,                                                                               
+,                                                                               
+,                                                                               
+,                                           .......                             
+,                                   .*#%%&&&%%&&&&&&&%&&%#*.                    
+,                               .(&&%%/,                ,(#%&%/.                
+,                            .(&%%/...                      ./%%%(.             
+,                          ,%%%(,,,*,*,,....................   .(%&#,           
+,                        .#%%##%%%%%%%%####/,,,,,,,,,,,,,*//(((((#%%&#.         
+,                       *%&%%/*//////((%&@@@&&#****,*#%&&&&&&&&&%%##&%%*        
+,                      *&&%%#////(/,......./&@@@#**%@@@&(,...,*****(%&&&/       
+,                     .&&%%%#((((,.     .*****#@@(&@@(.       ..*(/(%%&&%,      
+,              *//*.  #&&%%%%###*     *%****/(%,*#*,((*,/(%*    ./##%%&&&(.     
+,            .#//((#..#&%%%%%%%&/    .#*%@@@@(#(,%,/,&@@@&##,   ./%&%%&&&%.     
+,            *%%%%%# .%&&%%%%%%%%*.   *(*#&#,(#*#%%*,*%&%*#(  ..*%%%%%%&&%.     
+,         . ./%#%%%(..#&&&%%%%%%%%&(,....,*,,/%%##%%%/***,...,*%&%%%%%&&&#.     
+,  ./(*(%%%%%%%%%%%*  *&&&%%%%%%%%%%&&&&&&&&%%%######%&&&&&&&&%%%%%%%%&&&*      
+, ,%&&&%%%%%%#%%&%%/   (%&%%%%%%%%%%%%%%%%%%%%%#########%%%%%%%%%%%%%&&&#       
+, /&%%%%%%%%/*(##%%%&%(.(&&&%%%%%%%%%%&%#(((#((%&%%%%%%%%%%%%%%%%%%&&&&(        
+, *&%%#%#%#(%%//(#%%%&&# *&&&%%%%%%&#/*,**,,,.. .,.*%&&%&&&&&&%&%%&&&&*         
+, .%%/*/%(/*%%%#%%%%%%#.  .#&&&&%&&%(/,,*,,,...    ....,,**,/(#&&&&&(           
+,  ,&&&&&&&%%%%%%%%%%,       /&&&&&&&%%%%%%%%%%%%%%%%%%%%%%&&&&&&&/             
+,    *%(//((##%%%&%(           .#&&&%&&&&&&&&&&&&%&&&&&&&&&&&&&(.               
+,        ,*****,,.  ....,,,***////(#%&&&&&&&&&&&&&&&&&&&&&&%#(////***,,,....    
+,                    ...,,,****////((((###%%%%&&&&%%%%###((((///*****,,....     
+,                                  .........,,,,,,,,,,,...... .                 
+,                                                                               
+,                                                                               
+,                                                                               
+
+
+
+
+""")
+time.sleep(1)
+string = "Ddox done" 
+
+for letter in string:
+  sleep(0.01)
+  sys.stdout.write(letter)
+  sys.stdout.flush()
+
+time.sleep(2)
+
+string = "\n swat team and pizzas sent" 
+
+for letter in string:
+  sleep(0.01)
+  sys.stdout.write(letter)
+  sys.stdout.flush()
+
+
+time.sleep(2)
+
+replit.clear()
+print("program ending")
+time.sleep(5)
+print("goodbye nigga")
+time.sleep(1)
+
+
+
+
